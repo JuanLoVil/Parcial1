25bd6f545ee20020ba3bab8f57c1ea86a7772c4c
JuanLoVil <47319172+JuanLoVil@users.noreply.github.com>
2019-04-09 12:32:09 -0500

    Add files via upload

---
+++ Tarea 11/4/Tarea11-4/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tarea11_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonAfuera_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Boton de afuera clic");
+        }
+
+        private void BotonAdentro_Click(object sender, RoutedEventArgs e)
+        {
+            this.Title = "Titulo otro";
+        }
+    }
+}
