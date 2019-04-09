c90798760f6a92194f8d29f7a4c7aa84779fd34a
JuanLoVil <47319172+JuanLoVil@users.noreply.github.com>
2019-04-09 12:23:57 -0500

    Add files via upload

---
+++ Tarea 9/CSharp/Tarea9-Facade/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea9_Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAcelerador acelerador = new Acelerador();
+            IEmbrague embrague = new Embrague();
+            IPalancaCambios palancaCambios = new PalancaCambios();
+
+            Centralita centralita = new Centralita(embrague, acelerador, palancaCambios);
+            centralita.AumentarMarcha();
+        }
+    }
+}
