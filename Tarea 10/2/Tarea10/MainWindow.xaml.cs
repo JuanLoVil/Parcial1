1a99b496663e1f7793290c5845b1dd814651cbf9
JuanLoVil <47319172+JuanLoVil@users.noreply.github.com>
2019-04-09 12:26:21 -0500

    Add files via upload

---
+++ Tarea 10/2/Tarea10/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tarea10.Model;
+
+namespace Tarea10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string Name = "Hola mundo";
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyModelObject button1DataContext = new MyModelObject() { Name = "Soy el boton 1" };
+            MyModelObject button2DataContext = new MyModelObject() { BadName = "Soy el boton 12" };
+
+            boton1.DataContext = button1DataContext;
+            boton2.DataContext = button2DataContext;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainGridPanel.Background = Brushes.Purple;
+        }
+    }
+}
---
+++ Tarea 10/2/Tarea10/Model/MyModelObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea10.Model
+{
+    public class MyModelObject
+    {
+        public string Name { get; set; }
+        public string BadName { get; set; }
+    }
+}
